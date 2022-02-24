4ede8d1ecacdb370f86b61b85cc6b1376e863f2c
songlujie <1405784613@qq.com>
2022-02-24 19:19:54 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="increase">+1</button>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref, computed, reactive, toRefs } from "vue";
+
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
+  setup() {
+    // const count = ref(0);
+    // const double = computed(() => {
+    //   return count.value * 2;
+    // });
+    // const increase = () => {
+    //   count.value++;
+    // };
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+    });
+
+    const refData = toRefs(data);
+    return {
+      ...refData
+    };
+  },
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 });
 </script>
 
